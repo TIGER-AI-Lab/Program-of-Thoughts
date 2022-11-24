3c3cc735c7b86e4f7d7e06ad4879013c3f2dd207
Wenhu Chen <wenhuchen@uwaterloo.ca>
2022-11-24 15:45:05 -0500

    updating svamp results

--- run_svamp.py
+++ run_svamp.py
@@ -29,43 +29,53 @@ with open('data/SVAMP.json') as f:
 prompt_4shot = """
 Read the following passages to answer questions with Python code, store the result as a 'ans' variable:
 
-# Passage: Each pack of dvds costs 76 dollars. If there is a discount of 25 dollars on each pack
-# Question: How much do you have to pay to buy each pack?
-original_price = 76
-discount = 25
-ans = original_price - discount
+# Passage: James bought 93 red and 10 blue stickers, he used 31 red sticker on his fridge and 7 blue stickers on his laptop.
+# Question: How many red stickers does James have?
+original_red_stickers = 93
+used_red_stickers = 31
+ans = original_red_stickers - used_red_stickers
 
-# Passage: Paco had 26 salty cookies and 17 sweet cookies. He ate 14 sweet cookies and 9 salty cookies.
-# Question: How many salty cookies did Paco have left?
-original_salty_cookies = 26
-eaten_salty_cookies = 9
-ans = original_salty_cookies - eaten_salty_cookies
+# Passage: Allen went to supermarket to buy eggs, each egg costs 80 dollars, if the discount is 29 dollars.
+# Question: How much do you have to pay to buy for each egg?
+original_egg_price_in_dollars = 80
+discount_dollars = 29
+ans = original_egg_price_in_dollars - discount_dollars
 
-# Passage: Haley grew 9 trees in her backyard. After a typhoon 4 died. Then she grew 5 more trees.
-# Question: How many trees does she have left?
-original_trees = 9
-died_trees = 4
-regrew_trees = 5
-ans = original_trees - died_trees + regrew_trees
+# Passage: Dianna collects both cases and books. He bought 22 cases and 5 books from the store. Now he has 57 cases and 25 books.
+# Question: How many books did danny have at first?
+num_books_bought_at_store = 5
+num_books_now = 25
+ans = num_books_now - num_books_bought_at_store
 
-# Passage: Julia played tag with 5 kids on monday, 9 kids on tuesday and 15 kids on wednesday.
-# Question: How many kids did she play with on monday and wednesday?
-num_kids_on_monday = 5
-num_kids_on_wednesday = 15
-ans = num_kids_on_monday + num_kids_on_wednesday
+# Passage: There were 108 chickens and 20 sheeps at the farm, some of chickens and sheeps were sold. There are 87 chickens and 18 sheeps left now.
+# Question: How many chickens were sold?
+num_chicken_before = 108
+num_chicken_now = 87
+ans = num_chicken_before - num_chicken_now
 
-# Passage: Danny collects bottle caps and wrappers. He found 22 bottle caps and 30 wrappers at the park. Now he has 17 bottle caps and 57 wrappers in his collection.
-# Question: How many wrappers did danny have at first?
-num_wrappers_now = 57
-found_wrappers = 30
-ans = num_wrappers_now - found_wrappers
+# Passage: Katty scored 2 goals on monday, 8 goals on tuesday and 9 goals on wednesday.
+# Question: How many did Katty score on monday and wednesday?
+num_goals_on_monday = 2
+num_goals_on_wednesday = 9
+ans = num_goals_on_monday + num_goals_on_wednesday
 
-# Passage: There were 16 roses and 3 orchids in the vase. Jessica cut some more roses and orchids from her flower garden. There are now 7 orchids and 13 roses in the vase.
-# Question: How many orchids did she cut?
-num_orchids_now = 13
-num_orchids_before = 3
-ans = num_orchids_now - num_orchids_before
+# Passage: There are 5 girls and 4 boys in the Masquerade, 12 more girls and 7 more boys joined. 
+# Question: How many more girls than boys are in the Masquerade?
+num_girls_before = 5
+num_girls_joined = 12
+num_boys_before = 4
+num_boys_joined = 7
+total_girls = num_girls_before + num_girls_joined
+total_boys = num_boys_before + num_boys_joined
+ans = total_girls - total_boys
+
+# Passage: Joseph and Getty went to buy ice creams, they together bought 36 ice creams. On the way back, Joseph ate 12 of the ice creasm, and he has 2 ice creams left now. 
+# Question: How much ice creasm did Getty purchase?
+num_ice_creams_bought_by_joseph = 2 + 12
+total_ice_creams = 36
+ans = total_ice_creams - num_ice_creams_bought_by_joseph
 """
+
 
 if __name__ == "__main__":
     now = datetime.now()
@@ -136,7 +146,7 @@ if __name__ == "__main__":
             ans = safe_execute(r)
             ans = floatify_ans(ans)
             if ans is not None:
-                result_counter.update([ans])
+                result_counter.update([abs(ans)])
 
         if len(result_counter) > 0:
             prediction = result_counter.most_common(1)[0][0]
